41fcbba5c040b9df5b692f8800503d5366cb31cb
Ian Guen <narukunegu@gmail.com>
2026-09-10 10:15:38 +0700

    change config server

--- server/config.js
+++ server/config.js
@@ -2,8 +2,8 @@ module.exports = {
   version: "26.9.10",
   port: 6314,
   sql: {
-    user: "dcvlib",
-    password: "dcv1903",
+    user: "webA",
+    password: "@bcD1234",
     server: "localhost",
     database: "DataThuVien",
     port: 1433,
